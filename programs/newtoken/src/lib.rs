ddf8f85c813630ace7a75886f116a0bdf3b1eccd
chirag-empiric <chirag@empiricinfotech.com>
2024-07-18 17:18:13 +0530

    feat: initial commit

---
+++ programs/newtoken/src/lib.rs
@@ -0,0 +1,117 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
+        Metadata as Metaplex,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+declare_id!("7SWQTWv4NAAN53zidepsVPYEAAdK7kuSGM83m6NsnGw8");
+
+#[program]
+mod newtoken {
+    use super::*;
+
+    pub fn initiate_token(_ctx: Context<InitToken>) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[_ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+        let token_data: DataV2 = DataV2 {
+            name: "Net2Dev SPL Rewards Token".to_string(),
+            symbol: "N2DR".to_string(),
+            uri: "https://arweave.net/Xjqaj_rYYQGrsiTk9JRqpguA813w6NGPikcRyA1vAHM".to_string(),
+            seller_fee_basis_points: 200,
+            creators: None,
+            collection: None,
+            uses: None,
+        };
+
+        let metadata_ctx = CpiContext::new_with_signer(
+            _ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                payer: _ctx.accounts.payer.to_account_info(),
+                update_authority: _ctx.accounts.mint.to_account_info(),
+                mint: _ctx.accounts.mint.to_account_info(),
+                metadata: _ctx.accounts.metadata.to_account_info(),
+                mint_authority: _ctx.accounts.mint.to_account_info(),
+                system_program: _ctx.accounts.system_program.to_account_info(),
+                rent: _ctx.accounts.rent.to_account_info(),
+            },
+            &signer,
+        );
+
+        create_metadata_accounts_v3(metadata_ctx, token_data, false, true, None)?;
+
+        msg!("Token mint created successfully.");
+        Ok(())
+    }
+
+    pub fn mint_tokens(ctx: Context<MintTokens>, quantity: u64) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &signer,
+            ),
+            quantity,
+        )?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitToken<'info> {
+    #[account(mut)]
+    /// CHECK: UncheckedAccount
+    pub metadata: UncheckedAccount<'info>,
+    #[account(
+        init,
+        seeds = [b"mint"],
+        bump,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+}
+
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
+    )]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+pub struct Initialize {}
---
+++ scripts/transfer.js
@@ -0,0 +1,75 @@
+const { Keypair, Transaction, Connection, PublicKey } = require("@solana/web3.js");
+const { getAssociatedTokenAddress, createTransferCheckedInstruction, createAssociatedTokenAccountInstruction, getOrCreateAssociatedTokenAccount } = require("@solana/spl-token");
+const { bs58 } = require("@coral-xyz/anchor/dist/cjs/utils/bytes");
+
+const owner = "3athqFzWSks9otL3rZgQHoQLGS9iZvoLcBmrMu2BWjcfBwNutUcHYp1PfN1wZ9FtLfQ5MEYbKnNjVSHDG9p3gTMz" // private key string previously obtained 
+const spltoken = new PublicKey("2obqhHtJKVxxTQyhpjxakREwYjyxWbpEwSHQ83USeRmc"); // Program Id of your SPL Token, obtained during "anchor deploy"
+const sourceWallet = Keypair.fromSecretKey(bs58.decode(owner));
+const connection = new Connection("https://api.devnet.solana.com");
+
+const destWallet = new PublicKey("AZCMD8PAuRW9RqVksLxztTiUobbxFw3xq6Cf8oUVi9ve");
+const tokens = 2; // set the amount of tokens to transfer.
+
+async function genAta() {
+    let ata = await getAssociatedTokenAddress(
+        spltoken,
+        destWallet,
+        false
+    );
+    let tx = new Transaction();
+    tx.add(
+        createAssociatedTokenAccountInstruction(
+            sourceWallet.publicKey,
+            ata,
+            destWallet,
+            spltoken
+        )
+    );
+    console.log(`create ata txhash: ${await connection.sendTransaction(tx, [sourceWallet])}`);
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    return true;
+}
+
+const solanaTransferSpl = async () => {
+    let amount = tokens * 10 ** 9;
+    let sourceTokenRaw = await getAssociatedTokenAddress(
+        spltoken,
+        sourceWallet.publicKey,
+        false
+    );
+
+    let destTokenRaw = await getAssociatedTokenAddress(
+        spltoken,
+        destWallet,
+        false
+    );
+    let sourceATA = sourceTokenRaw.toBase58();
+    let destATA = destTokenRaw.toBase58();
+    try {
+        let transaction = new Transaction();
+        transaction.add(
+            createTransferCheckedInstruction(
+                new PublicKey(sourceATA),
+                spltoken,
+                new PublicKey(destATA),
+                sourceWallet.publicKey,
+                amount,
+                9
+            )
+        )
+        let tx = await connection.sendTransaction(transaction, [sourceWallet])
+        console.log('Tokens transferred Successfully, Receipt: ' + tx);
+        return;
+    }
+    catch (error) {
+        let generateAta = await genAta();
+        if (generateAta) {
+            await new Promise((resolve) => setTimeout(resolve, 15000));
+            solanaTransferSpl();
+            return;
+        }
+    };
+}
+
+
+solanaTransferSpl();
